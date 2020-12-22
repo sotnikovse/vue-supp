e36cd23d62c925a235d306b8b9f68873c0abaf31
Stepan Sotnikov <sotnikovse1207@gmail.com>
2020-12-23 02:12:58 +0900

    fix(build): rollup config

---
+++ packages/uipart/rollup.config.js
@@ -0,0 +1,83 @@
+import ts from 'rollup-plugin-typescript2'
+import json from '@rollup/plugin-json'
+import pkg from './package.json'
+
+const banner = `/*!
+  * ${pkg.name} v${pkg.version}
+  * (c) ${new Date().getFullYear()}
+  * Released under the ${pkg.license} License.
+  */`
+
+const outputConfigs = {
+  'esm': {
+    file: pkg.module,
+    format: `es`
+  },
+  cjs: {
+    file: pkg.main,
+    format: `cjs`
+  },
+}
+
+const packageConfigs = ['esm', 'cjs'].map(format => createConfig(format, outputConfigs[format]))
+
+export default packageConfigs
+
+function createConfig (format, output, plugins = []) {
+  if (!output) {
+    console.log(require('chalk').yellow(`invalid format: "${format}"`))
+    process.exit(1)
+  }
+
+  output.banner = banner
+
+  const tsPlugin = ts({
+    check: false,
+    tsconfig: './tsconfig.json',
+    cacheRoot: './node_modules/.rts2_cache',
+    tsconfigOverride: {
+      compilerOptions: {
+        sourceMap: true,
+        declaration: true,
+      },
+      exclude: ['node_modules', '**/__tests__']
+    }
+  })
+
+  const external = [
+    'vue',
+    '@popperjs/core',
+    'tslib',
+  ]
+
+  const nodePlugins =
+    format !== 'cjs'
+      ? [
+          require('@rollup/plugin-node-resolve').nodeResolve({
+            preferBuiltins: true
+          }),
+          require('@rollup/plugin-commonjs')({
+            sourceMap: false
+          }),
+          require('rollup-plugin-node-builtins')(),
+          require('rollup-plugin-node-globals')()
+        ]
+      : []
+
+  return {
+    input: './src/index.ts',
+    external,
+    plugins: [
+      json({
+        namedExports: false
+      }),
+      tsPlugin,
+      ...nodePlugins,
+      ...plugins
+    ],
+    output,
+    treeshake: {
+      moduleSideEffects: false
+    }
+  }
+}
