6517085231842fa343f11f0437c84281f0af4738
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-08-13 00:56:52 +0900

    feat: added render use-case

---
+++ packages/vue-supp/src/use/render.ts
@@ -0,0 +1,8 @@
+import { getCurrentInstance } from 'vue'
+
+import type { VNode } from 'vue'
+
+export function useRender(render: () => VNode): void {
+  const vm = getCurrentInstance() as any
+  vm.render = render
+}
