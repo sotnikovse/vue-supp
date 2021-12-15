b59ee96f48bc9a9f71a3b481314464016073f076
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-12-16 06:24:18 +0900

    feat: removed render use

--- packages/vue-supp/src/use/render.ts
+++
@@ -1,8 +0,0 @@
-import { getCurrentInstance } from 'vue'
-
-import type { VNode } from 'vue'
-
-export function useRender(render: () => VNode): void {
-  const vm = getCurrentInstance() as any
-  vm.render = render
-}
