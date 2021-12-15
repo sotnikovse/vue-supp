720eea5e11c0f968f9ae73a8e01a1bfe0857565b
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-12-16 07:20:52 +0900

    chore: use getCurrentInstance util

--- packages/vue-supp/src/use/model.ts
+++ packages/vue-supp/src/use/model.ts
@@ -1,12 +1,11 @@
-import { ref, watch, getCurrentInstance, toRef, Ref, warn } from 'vue'
+import { ref, watch, toRef, Ref } from 'vue'
+import { getCurrentInstance } from '../utils'
 
 export function useModel<
   Props extends object, // eslint-disable-line @typescript-eslint/ban-types
   Prop extends Extract<keyof Props, string>
 >(props: Props, prop: Prop) {
-  const vm = getCurrentInstance()
-
-  if (!vm) warn('useModel must be called from inside a setup function')
+  const vm = getCurrentInstance('useModel')
 
   const proxy = ref(props[prop]) as Ref<Props[Prop]>
 
