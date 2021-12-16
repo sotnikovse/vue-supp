8bdb16f1593fb53bcbd9593a445d7832605e1183
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-12-17 04:39:47 +0900

    refactor: resizeObserver

--- packages/vue-supp/src/composables/resizeObserver.ts
+++ packages/vue-supp/src/composables/resizeObserver.ts
@@ -1,86 +1,56 @@
-import { watch, onUnmounted, unref } from 'vue'
-import { MaybeRef } from '../../types'
+import { onBeforeUnmount, readonly, ref, watch } from 'vue'
+import type { DeepReadonly, Ref } from 'vue'
 
-export interface ResizeObserverSize {
-  readonly inlineSize: number
-  readonly blockSize: number
+interface ResizeState {
+  resizeRef: Ref<Element | undefined>
+  contentRect: DeepReadonly<Ref<DOMRectReadOnly | undefined>>
+  borderBoxSize: DeepReadonly<Ref<ResizeObserverSize | undefined>>
+  contentBoxSize: DeepReadonly<Ref<ResizeObserverSize | undefined>>
 }
 
-export interface ResizeObserverEntry {
-  readonly target: Element
-  readonly contentRect: DOMRectReadOnly
-  readonly borderBoxSize?: ReadonlyArray<ResizeObserverSize>
-  readonly contentBoxSize?: ReadonlyArray<ResizeObserverSize>
-  readonly devicePixelContentBoxSize?: ReadonlyArray<ResizeObserverSize>
-}
-
-// eslint-disable-next-line no-use-before-define
-export type ResizeObserverCallback = (
-  entries: ReadonlyArray<ResizeObserverEntry>,
-  observer: ResizeObserver
-) => void
-
-export interface ResizeObserverOptions {
-  /**
-   * Sets which box model the observer will observe changes to. Possible values
-   * are `content-box` (the default), and `border-box`.
-   *
-   * @default 'content-box'
-   */
-  box?: 'content-box' | 'border-box'
-}
-
-declare class ResizeObserver {
-  constructor(callback: ResizeObserverCallback)
-  disconnect(): void
-  observe(target: Element, options?: ResizeObserverOptions): void
-  unobserve(target: Element): void
-}
-
-/**
- * Reports changes to the dimensions of an Element's content or the border-box
- *
- * @param target
- * @param callback
- * @param options
- */
 export function useResizeObserver(
-  target: MaybeRef<Element | null | undefined>,
-  callback: ResizeObserverCallback,
-  options: ResizeObserverOptions = {}
-) {
-  let observer: ResizeObserver | undefined
-  const isSupported = window && 'ResizeObserver' in window
+  callback?: ResizeObserverCallback
+): ResizeState {
+  const resizeRef = ref<Element>()
+  const contentRect = ref<DOMRectReadOnly>()
+  const contentBoxSize = ref<ResizeObserverSize>()
+  const borderBoxSize = ref<ResizeObserverSize>()
 
-  const cleanup = () => {
-    if (observer) {
-      observer.disconnect()
-      observer = undefined
-    }
-  }
+  const observer = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+    callback?.(entries, observer)
 
-  const stopWatch = watch(
-    () => unref(target),
-    (el) => {
-      cleanup()
+    if (!entries.length) return
 
-      if (isSupported && window && el) {
-        observer = new window.ResizeObserver(callback)
-        observer!.observe(el, options) // eslint-disable-line @typescript-eslint/no-non-null-assertion
+    contentRect.value = entries[0].contentRect
+    contentBoxSize.value = entries[0].contentBoxSize[0]
+    borderBoxSize.value = entries[0].borderBoxSize[0]
+  })
+
+  onBeforeUnmount(() => {
+    observer.disconnect()
+  })
+
+  watch(
+    resizeRef,
+    (newValue, oldValue) => {
+      if (oldValue) {
+        observer.unobserve(oldValue)
+        contentRect.value = undefined
+        contentBoxSize.value = undefined
+        borderBoxSize.value = undefined
       }
+
+      if (newValue) observer.observe(newValue)
     },
-    { immediate: true, flush: 'post' }
+    {
+      flush: 'post',
+    }
   )
 
-  const stop = () => {
-    cleanup()
-    stopWatch()
-  }
-
-  onUnmounted(stop)
-
   return {
-    isSupported,
-    stop,
+    resizeRef,
+    contentRect: readonly(contentRect),
+    contentBoxSize: readonly(contentBoxSize),
+    borderBoxSize: readonly(borderBoxSize),
   }
 }
