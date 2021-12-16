72d8b4dca4818df85e8a54730fc6a5cfb09b9526
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-12-17 03:50:59 +0900

    fix: kebabCase

--- packages/vue-supp/src/utils/kebabCase.ts
+++ packages/vue-supp/src/utils/kebabCase.ts
@@ -4,5 +4,8 @@
  * @category String
  */
 export function kebabCase(str: string) {
-  return str.replace(/([A-Z])/g, (match) => `-${match.toLowerCase()}`)
+  return str
+    .replace(/[^a-z]/gi, '-')
+    .replace(/\B([A-Z])/g, '-$1')
+    .toLowerCase()
 }
