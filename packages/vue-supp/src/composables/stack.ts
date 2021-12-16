bc26d943e0c503deb5055fd4148321f41973b30f
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-12-17 04:06:51 +0900

    feat: added stack

---
+++ packages/vue-supp/src/composables/stack.ts
@@ -0,0 +1,40 @@
+// https://github.com/vuetifyjs/vuetify/blob/next/packages/vuetify/src/composables/stack.ts
+
+import { computed, effectScope, onScopeDispose, ref, toRaw, watch } from 'vue'
+import { getCurrentInstance } from '../utils'
+
+import type { ComponentInternalInstance, EffectScope, Ref } from 'vue'
+
+const stack = ref<ComponentInternalInstance[]>([])
+
+export function useStack(isActive: Ref<boolean>) {
+  const vm = getCurrentInstance('useStack')
+  let scope: EffectScope | undefined
+  watch(
+    isActive,
+    (val) => {
+      if (val) {
+        scope = effectScope()
+        scope.run(() => {
+          stack.value.push(vm)
+
+          onScopeDispose(() => {
+            const idx = stack.value.indexOf(vm)
+            stack.value.splice(idx, 1)
+          })
+        })
+      } else {
+        scope?.stop()
+      }
+    },
+    { immediate: true }
+  )
+
+  const isTop = computed(() => {
+    return toRaw(stack.value[stack.value.length - 1]) === vm
+  })
+
+  return {
+    isTop,
+  }
+}
